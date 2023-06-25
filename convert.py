c36135b9c3e1e940cbb10bcf78aa1b737378c248
cmolarti <cmolarti@gmail.com>
2023-06-25 16:48:03 -0400

    basic gui app in dark mode

--- convert.py
+++ convert.py
@@ -1,1 +1,31 @@
-import customtkinter
+import customtkinter as ctk
+import darkdetect
+
+
+class App(ctk.CTk):
+    def __init__(self, dark, width=300, height=400):
+        super().__init__()
+        self.title('Converter App')
+        self.gui_geometry(width, height)
+
+        if dark:
+            ctk.set_appearance_mode('dark')
+
+        self.bind('<Shift-Escape>', quit)
+        self.mainloop()
+    
+    def gui_geometry(self, width, height):
+        pWidth = width
+        pHeight = height
+        sWidth = self.winfo_screenwidth()
+        sHeight = self.winfo_screenheight()
+        mWidth = sWidth//2 - pWidth//2
+        mHeight = sHeight//2 - pHeight//2
+
+        self.geometry(f'{pWidth}x{pHeight}+{mWidth}+{mHeight}')
+
+
+if __name__ == '__main__':
+    dark = not darkdetect.isDark()
+    
+    app = App(dark)
