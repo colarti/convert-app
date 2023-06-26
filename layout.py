8c24f45fdbb5071fb607e438de1ee644fd6592a2
cmolarti <cmolarti@gmail.com>
2023-06-26 01:40:01 -0400

    created the conversion function, but can only update 1 way

--- layout.py
+++ layout.py
@@ -5,14 +5,14 @@ class Layout(ctk.CTkFrame):
     def __init__(self, parent, valueA, entryA, valueB, entryB):
         super().__init__(parent, fg_color='transparent')
 
-        TextOptionPanel(self, valueA, entryA)
-        TextOptionPanel(self, valueB, entryB)
+        TextOptionPanel(self, valueA, entryA, 'normal')
+        TextOptionPanel(self, valueB, entryB, 'disabled')
 
-        self.place(relx=.5, rely=.5, anchor='center')
+        self.place(relx=.5, rely=.5, anchor='center', relwidth=1)
 
 
 class TextOptionPanel(ctk.CTkFrame):
-    def __init__(self, parent, options, entry):
+    def __init__(self, parent, options, entry, state):
         super().__init__(parent, fg_color='grey')
 
         self.rowconfigure(0, weight=1)
@@ -21,7 +21,7 @@ class TextOptionPanel(ctk.CTkFrame):
         self.optionMenu = options
         self.entry = entry
         
-        ctk.CTkEntry(self, textvariable=self.entry, height=70, font=('Helvetica', 60)).grid(row=0, column=0, columnspan=2, sticky='ew', padx=5)
+        ctk.CTkEntry(self, textvariable=self.entry, state=state, height=70, font=('Helvetica', 60)).grid(row=0, column=0, columnspan=2, sticky='ew', padx=5)
         ctk.CTkOptionMenu(self, variable=self.optionMenu, values=list_distance).grid(row=0, column=2, sticky='ew')
 
 
